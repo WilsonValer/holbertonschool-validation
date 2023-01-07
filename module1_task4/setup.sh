7579c65ae01a817afe0a9993147c697d1c820879
WilsonValer <nederig21@gmail.com>
2023-01-07 16:22:16 -0500

    edited task04

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -4,7 +4,10 @@
 
 ##  The production website is hosted in  an Ubuntu 18.04 Docker container
 ##  The applications "GoHugo" and "Make" are installed with
-apt-get update && apt-get install -y hugo make
+apt-get update
+apt-get upgrade
+apt-get install -y wget
+apt-get install -y hugo make
 ##  When running the command
 wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb
 dpkg -i hugo_extended_0.84.0_Linux-64bit.deb
