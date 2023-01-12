1c5c419e9cc20d8bd807ff7eb77dce082008866b
WilsonValer <nederig21@gmail.com>
2023-01-12 11:37:30 -0500

    was edited script setup

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -4,6 +4,7 @@
 apt-get update && apt-get install -y make wget
 wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb
 sudo dpkg -i hugo_extended_0.84.0_Linux-64bit.deb
+
 # Install markdownlint and zip tools
 apt-get install zip
 npm install -g markdownlint-cli -y
@@ -12,11 +13,9 @@ npm install -g markdownlint-cli -y
 sudo apt-get remove golang-go
 rm -rf /usr/local/go || true
 
-# Delete dist folder
-rm -rf dist/ 2> /dev/null
 
 # clean enviroment file and directory
-
-rm -rf awesome-api || true
-rm -rf coverage-units.out || true
-rm -rf coverage-integrations.out || true
+rm -rf dist/ 2> /dev/null
+rm -f awesome-api || true
+rm -f coverage-units.out || true
+rm -f coverage-integrations.out || true
