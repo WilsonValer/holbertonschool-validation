ec97eb5d5ff4d6711370c3e83bee359f3069c6ad
WilsonValer <nederig21@gmail.com>
2023-01-13 14:45:51 -0500

    edited

--- module4_task1/setup.sh
+++ module4_task1/setup.sh
@@ -2,8 +2,8 @@
 
 # Install appropriate version of hugo to be run in docker container 'ubuntu 18.04'
 apt-get update && apt-get install -y make wget
-# wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb
-# sudo dpkg -i hugo_extended_0.84.0_Linux-64bit.deb
+wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb
+sudo dpkg -i hugo_extended_0.84.0_Linux-64bit.deb
 
 # Install markdownlint and zip tools
 apt-get install zip
