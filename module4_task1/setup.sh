affaf38381d125864f454595ffd98ecfde3e6046
WilsonValer <nederig21@gmail.com>
2023-01-18 20:50:02 -0500

    setup.sh

--- module4_task1/setup.sh
+++ module4_task1/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install appropriate version of hugo to be run in docker container 'ubuntu 18.04'
-apt-get update && apt-get install -y make wget
+# apt-get update && apt-get install -y make wget
 # wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb
 # sudo dpkg -i hugo_extended_0.84.0_Linux-64bit.deb
 
